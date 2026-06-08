ae057e3014ffe43439ec320b8ce76cfeba4cca12
amitchandradas2004 <amitcdas792@gmail.com>
2026-06-09 01:38:35 +0600

    Added search and filter option in the all jobs page

--- src/app/jobs/page.jsx
+++ src/app/jobs/page.jsx
@@ -1,11 +1,9 @@
-import JobCard from "@/components/jobs/JobCard";
+import JobListingContainer from "@/components/jobs/JobListingContainer";
 import { getJobs } from "@/lib/api/jobs";
-// import JobListingContainer from "@/components/jobs/JobListingContainer";
 
-export default async function AllJobs() {
+export default async function Page() {
   // Fetched server-side on the initial request
   const jobs = await getJobs();
-  // console.log(jobs, "All jobs");
 
   return (
     <div className="w-full min-h-screen bg-zinc-950 p-6 md:p-12 text-white">
@@ -14,15 +12,10 @@ export default async function AllJobs() {
         <p className="text-zinc-400 mt-2">
           Discover your next engineering challenge.
         </p>
-        {jobs.length}
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
-        {jobs && jobs.map((job) => <JobCard key={job._id} job={job} />)}
-
-        {/* Pass data to the Client Wrapper to handle filtering interactivity */}
-        {/* <JobListingContainer initialJobs={jobs || []} /> */}
-      </div>
+      {/* Pass data to the Client Wrapper to handle filtering interactivity */}
+      <JobListingContainer initialJobs={jobs || []} />
     </div>
   );
 }
